a3112a9cdba6734b391e1cb8832868568f6b5fd8
25lp5076 User <25lp5076@administrators-iMac.local>
2026-02-05 21:04:44 +0800

    small

--- src/app/_Components/UpComing.tsx
+++ src/app/_Components/UpComing.tsx
@@ -10,7 +10,7 @@ export const UpComing = () => {
           <ArrowRight className="size-4" />
         </button>
       </div>
-      <div className="grid  mx-auto grid-cols-2 place-content-between gap-5  sm:grid-cols-3 sm:gap-11 lg:gap-8 md:grid-cols-3 lg:grid-cols-4 md:gap-8 xl:gap-x-14 xl:grid-cols-5 2xl:gap-x-18">
+      <div className="grid  mx-auto grid-cols-2 place-content-between gap-5 sm:grid-cols-3 sm:gap-11 lg:gap-8 md:grid-cols-3 lg:grid-cols-4 md:gap-8 xl:gap-x-14 xl:grid-cols-5 2xl:gap-x-18">
         {Array.from({ length: 10 }).map((_, i) => (
           <MovieCard key={i} />
         ))}
